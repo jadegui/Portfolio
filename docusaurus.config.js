0d133980f7e5421edb00ea172cfed96abb7bc528
jadegui <jadeguinoiseau@gmail.com>
2025-01-30 11:46:03 -0500

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jadegui.github.io',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Portfolio/',
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jadegui', // Usually your GitHub org/user name.
   projectName: 'Portfolio', // Usually your repo name.
-  deploymentBranch: 'deployment',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
